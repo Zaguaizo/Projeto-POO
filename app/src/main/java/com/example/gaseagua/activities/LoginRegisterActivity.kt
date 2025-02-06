6a48ab02211c48ef77bb5d6f159c12f031eb5592
Zaguaizo <swencan.2@gmail.com>
2025-02-06 09:18:53 -0300

    Primeiro commit

---
+++ app/src/main/java/com/example/gaseagua/activities/LoginRegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gaseagua.activities
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.example.gaseagua.R
+
+class LoginRegisterActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_register)
+    }
+}
